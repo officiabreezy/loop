ead0690b4698feb07108de1adf893e5f2f5aeee7
officiabreezy <isaacsegun2055@gmail.comgit config --global user.email isaacsegun2055@gmail.com>
2023-06-15 06:10:31 -0700

    commit

--- backend.js
+++ backend.js
@@ -1,14 +1,14 @@
-const express = require("express")
-const app = express();
-const port =6500
+// const express = require("express")
+// const app = express();
+// const port =6500
 
-app.get('/', (req,res)=> {
-    res.send ("hello senior gurus")
-})
+// app.get('/', (req,res)=> {
+//     res.send ("hello senior gurus")
+// })
  
-app.listen(port,() => {
-    console.log(`server is running on http://localhost:${port}`);
-})
+// app.listen(port,() => {
+//     console.log(`server is running on http://localhost:${port}`);
+// })
 
  const express = require("express");
  const app = express();
@@ -19,3 +19,5 @@ app.listen(port,() => {
  app.listen(port,() => {
     console.log("server is running on port 4500");
  })
+
+  
